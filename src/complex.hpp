7e89e3722f2ce0f7f26009504237a22be2906a72
Alexandra <alexandragrecu984@yahoo.com>
2018-11-04 22:14:54 +0200

    Declare Complex class with its methods

---
+++ src/complex.hpp
@@ -0,0 +1,99 @@
+#ifndef COMPLEX_SRC_COMPLEX_HPP_
+#define COMPLEX_SRC_COMPLEX_HPP_
+
+#include <string>
+#include <iostream>
+
+namespace complex {
+
+class Complex {
+ public:
+  // constructors
+  Complex(double real = 0, double imaginary = 0);
+  Complex(std::string str);
+  Complex(const Complex& rhs);
+
+  // getters
+  double real() const;
+  double imaginary() const;
+
+  // setters
+  void set_real(double real);
+  void set_imaginary(double imaginary);
+
+  // assignment operators
+  Complex& operator=(const Complex& rhs);
+  Complex& operator=(double rhs);
+
+  Complex& operator+=(const Complex& rhs);
+  Complex& operator+=(double rhs);
+
+  Complex& operator-=(const Complex& rhs);
+  Complex& operator-=(double rhs);
+
+  Complex& operator*=(const Complex& rhs);
+  Complex& operator*=(double rhs);
+
+  Complex& operator/=(const Complex& rhs);
+  Complex& operator/=(double rhs);
+
+  // Unary operators
+  friend Complex operator+(const Complex& rhs);
+  friend Complex operator-(const Complex& rhs);
+
+  // Binary operators
+  Complex operator+(const Complex& rhs) const;
+  Complex operator+(double rhs) const;
+  friend Complex operator+(double lhs, const Complex& rhs);
+
+  Complex operator-(const Complex& rhs) const;
+  Complex operator-(double rhs) const;
+  friend Complex operator-(double lhs, const Complex& rhs);
+
+  Complex operator*(const Complex& rhs) const;
+  Complex operator*(double rhs) const;
+  friend Complex operator*(double lhs, const Complex& rhs);
+
+  Complex operator/(const Complex& rhs) const;
+  Complex operator/(double rhs) const;
+  friend Complex operator/(double lhs, const Complex& rhs);
+
+  friend Complex operator^(const Complex& lhs, int rhs);
+
+  // Relational operators
+  bool operator==(const Complex& rhs) const;
+  bool operator==(double rhs) const;
+  friend bool operator==(double lhs, const Complex& rhs);
+
+  bool operator!=(const Complex& rhs) const;
+  bool operator!=(double rhs) const;
+  friend bool operator!=(double lhs, const Complex& rhs);
+
+  // ToString method
+  std::string ToString() const;
+
+  // Returns conjugate of number
+  Complex Conjugate() const;
+
+  // conversion operators
+  operator std::string() const;
+
+  // Friend Methods
+  friend double Abs(const Complex& num);
+  friend Complex Sqrt(const Complex& num, int ord = 1);
+
+  // Read/Write operators
+  friend istream& operator>>(istream& in, Complex& num);
+  friend ostream& operator<<(ostream& out, const Complex& num);
+
+ private:
+  void SetFromString(std::string str);
+
+  double _real;
+  double _imaginary;
+};
+
+}
+
+#endif
+
