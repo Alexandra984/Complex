d66fdeff43826d77c15000b4b6ab18f36776315c
Alexandra <alexandragrecu984@yahoo.com>
2018-11-05 14:23:52 +0200

    Implement copy constructor

--- src/complex.cpp
+++ src/complex.cpp
@@ -7,4 +7,11 @@ Complex::Complex(double real, double imaginary) :
     _imaginary(imaginary) {
 }
 
+Complex::Complex(const Complex& rhs) :
+    _real(rhs._real),
+    _imaginary(rhs._imaginary) {
+}
+
+
+
 }
